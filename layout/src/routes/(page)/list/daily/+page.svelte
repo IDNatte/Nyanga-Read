c9fbfa018ab47fdc5be89cb3d47f9118c182bf9f
IDNatte <idnatte@gmail.com>
2023-03-26 22:35:54 +0800

    fixing some endpoint and add based on transalated language filter, remove debug message, bump version to version 1.0.0 stable

--- layout/src/routes/(page)/list/daily/+page.svelte
+++ layout/src/routes/(page)/list/daily/+page.svelte
@@ -45,7 +45,7 @@
 
 	async function getInitManga() {
 		let mangaData = await fetch(
-			`https://api.mangadex.org/manga?limit=9&offset=0&originalLanguage[]=ja&excludedTags[]=5920b825-4181-4a17-beeb-9918b0ff7a30&includes[]=cover_art&translatedLanguage[]=${document.documentElement.lang}`
+			`https://api.mangadex.org/manga?limit=9&offset=0&originalLanguage[]=ja&excludedTags[]=5920b825-4181-4a17-beeb-9918b0ff7a30&includes[]=cover_art&availableTranslatedLanguage[]=${document.documentElement.lang}`
 		);
 		if (mangaData.status === 200) {
 			let manga = await mangaData.json();
@@ -57,7 +57,7 @@
 
 	async function getManga(offset: number, limit: number) {
 		let mangaData = await fetch(
-			`https://api.mangadex.org/manga?limit=${limit}&offset=${offset}&originalLanguage[]=ja&excludedTags[]=5920b825-4181-4a17-beeb-9918b0ff7a30&includes[]=cover_art&translatedLanguage[]=${document.documentElement.lang}`
+			`https://api.mangadex.org/manga?limit=${limit}&offset=${offset}&originalLanguage[]=ja&excludedTags[]=5920b825-4181-4a17-beeb-9918b0ff7a30&includes[]=cover_art&availableTranslatedLanguage[]=${document.documentElement.lang}`
 		);
 
 		if (mangaData.status === 200) {
