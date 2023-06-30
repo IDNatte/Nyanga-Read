2c6af7105fe5620540a51ebcafd892bc435aa04f
IDNatte <idnatte@gmail.com>
2023-07-01 06:15:27 +0800

    fix error page transalation

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -7,10 +7,10 @@
 	<div class="flex items-center divide-x divide-slate-500">
 		<div class="homepage pb-5 pt-[4.5em] flex flex-col w-full h-screen items-center justify-center">
 			<span class="text-5xl pb-5">🙀</span>
-			<span class="uppercase">{$_('page.errorPage.message')}..!!</span>
+			<span class="uppercase">{$_('page.errorPage.notification')}..!!</span>
 			<div class="rounded bg-slate-800 w-[400px] h-[150px] mt-7 text-white flex flex-col">
 				<span class="block border-b py-2 pl-2 capitalize text-sm"
-					>{$_('page.errorPage.message')}</span
+					>{$_('page.errorPage.errMsg')}</span
 				>
 				<div class="w-full h-full flex items-center justify-center">
 					<pre class="p-5">{$page.status}:{$page.error?.message}</pre>
