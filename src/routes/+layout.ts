8d2655cdde8917a97332c2187a6810bc2e8337f0
IDNatte <idnatte@gmail.com>
2024-01-11 13:40:08 +0800

    fix build error for front-end

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -7,7 +7,7 @@ import '../app.css'
 import '$lib/i18n'
 
 export const ssr = false
-export const prerender = true
+// export const prerender = true
 
 
 export const load: LayoutLoad = async ({ fetch }) => {
